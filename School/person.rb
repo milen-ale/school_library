3cda2f4afacffd9a08aba92f802ed5866bfe3272
milen-ale <milenalebel2@gmail.com>
2022-11-01 13:42:06 +0300

    add person class

---
+++ School/person.rb
@@ -0,0 +1,26 @@
+class Person
+    attr_accessor :name, :age
+    attr_reader :id
+  
+    def initialize(age, name: 'unknown', parent_permission: true)
+      @id = Random.rand(1..1000)
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
+    end
+  
+    def age_verify
+      of_age? || @parent_permission
+    
+    end
+
+    private
+  
+    def of_age?
+      @age >= 18
+    end
+ 
+  end
+
+ 
+ 
