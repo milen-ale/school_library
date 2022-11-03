c1634c1adef60ab66eca7077138234b33098c013
milen-ale <milenalebel2@gmail.com>
2022-11-03 23:01:50 +0300

    fix rubocop linter errors

--- School/rental.rb
+++ School/rental.rb
@@ -1,22 +1,22 @@
 class Rental
-    attr_accessor :date
-    attr_reader :book, :person
-    
-    def initialize (date, book, person)
-        @date = date
-        @book = book
-        book.rental <<self
-        @person = person
-        person.rental <<self
-    end
+  attr_accessor :date
+  attr_reader :book, :person
 
-    def book=(book)
-        @book = book
-        book.rentals.push(self) unless book.rentals.include?(self)
-      end
-    
-      def person=(person)
-        @person = person
-        person.rentals.push(self) unless person.rentals.include?(self)
-      end
-    end
+  def initialize(date, book, person)
+    @date = date
+    @person = person
+    person.rental << self
+    @book = book
+    book.rental << self
+  end
+
+  def book=(book)
+    @book = book
+    book.rentals.push(self) unless book.rentals.include?(self)
+  end
+
+  def person=(person)
+    @person = person
+    person.rentals.push(self) unless person.rentals.include?(self)
+  end
+end
